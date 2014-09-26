42c532e9ff01780b859880a013791944401e5b84
Speransky Danil <speranskydanil@gmail.com>
2014-09-26 21:31:53 +0700

    include south

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Category'
+        db.create_table(u'blog_category', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=120)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'blog', ['Category'])
+
+        # Adding model 'Article'
+        db.create_table(u'blog_article', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Category'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=240)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'blog', ['Article'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Category'
+        db.delete_table(u'blog_category')
+
+        # Deleting model 'Article'
+        db.delete_table(u'blog_article')
+
+
+    models = {
+        u'blog.article': {
+            'Meta': {'object_name': 'Article'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['blog.Category']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '240'})
+        },
+        u'blog.category': {
+            'Meta': {'object_name': 'Category'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '120'})
+        }
+    }
+
+    complete_apps = ['blog']
