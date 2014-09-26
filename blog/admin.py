f8a4a692101f197b575ba84a4c597d6bfb3f925e
Speransky Danil <speranskydanil@gmail.com>
2014-09-26 22:47:20 +0700

    fix models and admin.py in order to display necessary data on the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from blog.models import Category, Article
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Article)
 
--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ class Category(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
 
+    def __unicode__(self):
+        return self.title
+
     class Meta:
         verbose_name_plural = 'categories'
 
@@ -11,4 +14,7 @@ class Article(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=240)
     content = models.TextField()
+
+    def __unicode__(self):
+        return self.title
 
