048cf453893cf7d54c53991969851e32b9e6f204
Speransky Danil <speranskydanil@gmail.com>
2014-09-28 12:19:37 +0700

    added show action for articles, changed styles

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'blog.views.index', name='index'),
+    url(r'^(\d+)$', 'blog.views.show', name='show'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 from blog.models import Article
 
+@require_GET
 def index(request):
     articles = Article.objects.select_related().all()
     return render(request, 'blog/index.html', { 'articles': articles })
+
+@require_GET
+def show(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'blog/show.html', { 'article': article })
 
