da2b839a102456bafc07cb66c96ffe72120466a5
Speransky Danil <speranskydanil@gmail.com>
2014-09-28 18:37:16 +0700

    fix js for the moving block (optimizing for small screens)

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -3,9 +3,17 @@ $(document).ready(function () {
     var block = $('.recent_articles_block');
 
     if ($(window).width() > 970 && $(window).scrollTop() > 1200) {
-      if (!block.hasClass('fixed')) block.hide().addClass('fixed').fadeIn();
+      if (!block.hasClass('fixed')) {
+        block
+          .width(block.width())
+          .hide()
+          .addClass('fixed')
+          .fadeIn();
+      }
     } else {
-      block.removeClass('fixed');
+      block
+        .removeClass('fixed')
+        .css('width', 'auto');
     }
   });
 });
