c46549e9c351cb368ec200a18c6ce92d30b97ac5
Speransky Danil <speranskydanil@gmail.com>
2014-09-28 13:28:46 +0700

    added pagination into the blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Article
 
 @require_GET
 def index(request):
     articles = Article.objects.select_related().all()
+    page = request.GET.get('page')
+    paginator = Paginator(articles, 20)
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/index.html', { 'articles': articles })
 
 @require_GET
