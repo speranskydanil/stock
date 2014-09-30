8cca466b2477972a3a797e772ce3794e3658e3d1
Speransky Danil <speranskydanil@gmail.com>
2014-09-30 15:45:14 +0700

    add profile with list of articles written by users

--- stock/views.py
+++ stock/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import login
+from django.contrib.auth.decorators import login_required
 from stock.models import Message
 from stock.forms import ContactForm, SignUpForm
+from blog.models import Article
 
 @require_GET
 def about(request):
@@ -51,4 +54,21 @@ def sign_up(request):
         form = SignUpForm()
 
     return render(request, 'registration/sign_up.html', { 'form': form })
+
+def paginated(objects, request):
+    paginator = Paginator(objects, 20)
+    page = request.GET.get('page')
+
+    try:
+        return paginator.page(page)
+    except PageNotAnInteger:
+        return paginator.page(1)
+    except EmptyPage:
+        return paginator.page(paginator.num_pages)
+
+@require_GET
+@login_required
+def profile(request):
+    articles = paginated(Article.objects.select_related().filter(author=request.user), request)
+    return render(request, 'profile.html', { 'articles': articles })
 
